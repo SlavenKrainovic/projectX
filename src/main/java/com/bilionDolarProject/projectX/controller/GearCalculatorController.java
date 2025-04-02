13dd92851059eb933f6791926e4b231782955df7
SlavenKrainovic <95292652+SlavenKrainovic@users.noreply.github.com>
2025-04-02 20:50:21 +0200

    refactored imports

--- src/main/java/com/bilionDolarProject/projectX/controller/GearCalculatorController.java
+++ src/main/java/com/bilionDolarProject/projectX/controller/GearCalculatorController.java
@@ -13,10 +13,19 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RestController
 @RequestMapping("/api/v1/gearbox")
--- src/main/java/com/bilionDolarProject/projectX/controller/GearboxCalculatorController.java
+++ src/main/java/com/bilionDolarProject/projectX/controller/GearboxCalculatorController.java
@@ -1,11 +1,9 @@
 package com.bilionDolarProject.projectX.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller

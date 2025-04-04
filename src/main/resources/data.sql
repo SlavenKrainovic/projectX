9243948755cec5378b4cfff5248cfa5855fc40ea
SlavenKrainovic <95292652+SlavenKrainovic@users.noreply.github.com>
2025-04-04 23:36:32 +0200

    added pre set gearboxes on start app

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRE_SET_GEARBOX ("ID", "NAME", "CAR_BRAND", "GEAR1", "GEAR2", "GEAR3", "GEAR4", "GEAR5", "GEAR6", "GEAR7", "FINAL_DRIVE") VALUES
+('1','Honda S80 5-Speed Manual', 'Honda', 3.230, 2.105, 1.458, 1.103, 0.848, 0, 0, 4.40),
+('2','Honda S4C 5-Speed LSD', 'Honda', 3.266, 1.904, 1.269, 0.972, 0.780, 0, 0, 4.40);

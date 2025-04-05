cb3cef93985c61ec399a0a02a6839acad04eb99f
SlavenKrainovic <95292652+SlavenKrainovic@users.noreply.github.com>
2025-04-05 19:24:45 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO PRE_SET_GEARBOX ("ID", "NAME", "CAR_BRAND", "GEAR1", "GEAR2", "GEAR3", "GEAR4", "GEAR5", "GEAR6", "GEAR7", "FINAL_DRIVE") VALUES
-('1','Honda S80 5-Speed Manual', 'Honda', 3.230, 2.105, 1.458, 1.103, 0.848, 0, 0, 4.40),
-('2','Honda S4C 5-Speed LSD', 'Honda', 3.266, 1.904, 1.269, 0.972, 0.780, 0, 0, 4.40);
+('1','S80 5-Speed Manual', 'Honda', 3.230, 2.105, 1.458, 1.103, 0.848, 0, 0, 4.40),
+('2','S4C 5-Speed LSD', 'Honda', 3.266, 1.904, 1.269, 0.972, 0.780, 0, 0, 4.40),
+('3','DQ250', 'Volkswagen', 3.46, 2.05, 1.30, 0.90, 0.91, 0.76, 0, 4.40),
+('4','DQ381/DQ500', 'Volkswagen', 3.75, 2.27, 1.53, 1.15, 0.86, 0.68, 0.56, 4.77);

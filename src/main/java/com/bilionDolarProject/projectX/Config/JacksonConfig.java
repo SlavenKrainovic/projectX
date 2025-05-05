b51cef5c546d1031beba44613f2d1cfa99a80e36
SlavenKrainovic <95292652+SlavenKrainovic@users.noreply.github.com>
2025-05-05 13:41:12 +0200

    update

--- src/main/java/com/bilionDolarProject/projectX/Config/JacksonConfig.java
+++ src/main/java/com/bilionDolarProject/projectX/Config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package com.bilionDolarProject.projectX.config;
+package com.bilionDolarProject.projectX.Config;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
--- src/main/java/com/bilionDolarProject/projectX/Config/WebConfig.java
+++ src/main/java/com/bilionDolarProject/projectX/Config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.bilionDolarProject.projectX.config;
+package com.bilionDolarProject.projectX.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
